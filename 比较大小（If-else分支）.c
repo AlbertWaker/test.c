1eedb857a5578e4f8a5c06a0df6e23b4064f81c4
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-02-17 21:32:36 +0800

    Add files via upload

---
+++ 比较大小（If-else分支）.c
@@ -0,0 +1,11 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int num1 = 10, num2 = 20;
+    if (num1 > num2)
+        printf("较大值是:%d", num1);
+    else
+        printf("较大值是:%d", num2);
+    return 0;
+}
