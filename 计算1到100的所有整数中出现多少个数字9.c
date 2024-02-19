a3eff0f20dfbb774c9c3e2c4a57b035f5d8bd1ea
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-02-19 19:35:23 +0800

    Add files via upload

---
+++ 计算1到100的所有整数中出现多少个数字9.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    int count = 0;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 10 == 9)
+        {
+            count++;
+        }
+        if (i / 10 == 9)
+        {
+            count++;
+        }
+    }
+    printf("count = %d\n", count);
+    return 0;
+}
