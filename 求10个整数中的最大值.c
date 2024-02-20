6492a7e1e175948d35591d9081b26507a293b228
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-02-20 18:57:32 +0800

    Add files via upload

---
+++ 求10个整数中的最大值.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+    int max = arr[0];//最大值
+    int i = 0;
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    for (i = 0; i < sz; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    printf("max = %d\n", max);
+    return 0;
+}
