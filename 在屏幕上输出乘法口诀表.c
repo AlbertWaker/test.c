c7753e630750598e40cabeb285d37a76113e60ef
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-02-20 18:58:14 +0800

    Add files via upload

---
+++ 在屏幕上输出乘法口诀表.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    //确定打印9行
+    for (i = 1; i <= 9; i++)
+    {
+        //打印一行
+        int j = 1;
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d*%d=%d\t", j, i, i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
