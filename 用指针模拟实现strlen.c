468bb81add0741b1bb5e63fcd2c07793d1c783f1
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-02-28 22:00:43 +0800

    Add files via upload

---
+++ 用指针模拟实现strlen.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+my_strlen(char* str)
+{
+    char* start = str;
+    char* end = str;
+    while (*end != '\0')
+    {
+        end++;
+    }
+    return end - start;
+}
+int main()
+{
+    char arr[] = "hello";
+    int len = my_strlen(arr);
+    printf("%d\n", len);
+    return 0;
+}
