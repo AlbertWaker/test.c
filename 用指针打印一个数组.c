dd8437d66d75d6e5494a134ea81c002d24bfe800
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-01 18:08:36 +0800

    Add files via upload

---
+++ 用指针打印一个数组.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    int i = 0;
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    int* p = arr;
+    for (i = 0; i < sz; i++)
+    {
+        printf("%d ", *p);
+        p++;
+    }
+    return 0;
+}
