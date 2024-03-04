c66baec0dc1d5f1fac989e228976fb8e308d3922
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-04 09:17:50 +0800

    Add files via upload

---
+++ BC68-X图案.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int n;
+    while (scanf("%d", &n) == 1)
+    {
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (i == j)
+                {
+                    printf("*");
+                }
+                else if (i + j == n - 1)
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
