9449b35655ab999901d2d63c4f9c63d7e652697b
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-04 09:15:18 +0800

    Add files via upload

---
+++ 获取一个整数二进制序列中所有的偶数位和奇数位，分别打印出二进制序列.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    int num = 0;
+    scanf("%d", &num);
+    //获取奇数位的数字
+    for (i = 30; i >= 0; i -= 2)
+    {
+        printf("%d ", (num >> i) & 1);
+    }
+    printf("\n");
+    //获取偶数位的数字
+    for (i = 31; i >= 0; i -= 2)
+    {
+        printf("%d ", (num >> i) & 1);
+    }
+    return 0;
+}
