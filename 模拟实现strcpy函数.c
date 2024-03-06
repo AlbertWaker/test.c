f8cbb80f65559bdf4103544522821114056a8337
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-06 23:45:01 +0800

    Add files via upload

---
+++ 模拟实现strcpy函数.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <assert.h>
+
+void my_strcpy(char* dest, const char* src)
+{
+	//断言
+	assert(src != NULL);
+	assert(dest != NULL);
+
+	while (*dest++ = *src++)
+	{
+		;
+	}
+}
+
+int main()
+{
+	char arr1[20] = "XXXXXXXXXXXX";
+	char arr2[] =   "hello bit";
+	my_strcpy(arr1, arr2);
+	printf("%s\n",arr1);
+	return 0;
+} 
