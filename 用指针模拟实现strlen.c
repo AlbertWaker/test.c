380bd0b0fc7778c5413e9957dddf900fb49bdd39
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-08 23:50:11 +0800

    Add files via upload

--- 用指针模拟实现strlen.c
+++ 用指针模拟实现strlen.c
@@ -1,19 +1,24 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
-my_strlen(char* str)
+#include <assert.h>
+
+int my_strlen(const char* str)
 {
-    char* start = str;
-    char* end = str;
-    while (*end != '\0')
-    {
-        end++;
-    }
-    return end - start;
+	int count = 0;
+	assert(str);
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
 }
+
 int main()
 {
-    char arr[] = "hello";
-    int len = my_strlen(arr);
-    printf("%d\n", len);
-    return 0;
+	char arr[] = "hello world";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
 }
