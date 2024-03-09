ba99747463c75cc247d31022b1270b6b18f8f83c
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-09 22:22:18 +0800

    Add files via upload

---
+++ BC51三角形判断.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	while (scanf("%d %d %d", &a, &b, &c) == 3)
+	{
+		//判断
+		if ((a + b > c) && (a + c > b) && (b + c > a))
+		{
+			if (a == b && b == c)
+			{
+				printf("Equilateral triangle!\n");//等边三角形
+			}
+			else if ((a == b && b != c) || (a == c && c != b) || (b == c && b != a))
+			{
+				printf("Isosceles triangle!\n");//等腰三角形
+			}
+			else
+			{
+				printf("Ordinary triangle!\n");//普通三角形
+			}
+		}
+		else
+		{
+			printf("Not a triangle!\n");//不是三角形
+		}
+	}
+	return 0;
+}
