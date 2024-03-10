e50bf7c76e9c9573f9f2ef18d3d5a5a17684f64d
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-10 21:01:17 +0800

    Add files via upload

---
+++ 将一个字符串str的内容颠倒过来，并输出.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	char arr[10001] = { 0 };//数据范围:1<=len(str)<=10000
+	gets(arr);
+	//逆序
+	int left = 0;
+	int right = strlen(arr)-1;
+
+	while (left < right)
+	{
+		char tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+
+		left++;
+		right--;
+	}
+	printf("%s\n", arr);
+	return 0;
+}
