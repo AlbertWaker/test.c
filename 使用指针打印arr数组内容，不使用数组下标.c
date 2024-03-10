a5e4264180a2b5ffe02fa4487502fbc87aa706a2
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-10 20:43:09 +0800

    Add files via upload

---
+++ 使用指针打印arr数组内容，不使用数组下标.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void print(int* p,int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
+	return 0;
+}
