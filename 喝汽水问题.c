fe4598657e3c9059091c39d128ddad6301caa5e1
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-13 09:04:43 +0800

    Add files via upload

---
+++ 喝汽水问题.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给n元，可以喝多少汽水
+#include <stdio.h>
+int main()
+{
+	int money = 0;
+	scanf("%d", &money);
+	int total = money;
+	int empty = money;
+	//置换
+	while (empty >= 2)
+	{
+		total += empty / 2;
+		empty = empty / 2 + empty % 2;
+	}
+	printf("%d\n", total);
+	return 0;
+}
