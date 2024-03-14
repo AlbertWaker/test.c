b957e8567cd953782449248940e2c5c4bc50fe65
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-14 17:49:16 +0800

    Add files via upload

---
+++ 求输入A和B的最小公倍数.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int m = (a > b ? a : b);
+//	while (1)
+//	{
+//		if (m % a == 0 && m % b == 0)
+//		{
+//			break;
+//		}
+//		else
+//			m++;
+//	}
+//	printf("%d\n", m);
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int i = 1;
+	while (a * i % b)
+	{
+		i++;
+	}
+	printf("%d\n", i*a);
+	return 0;
+}
