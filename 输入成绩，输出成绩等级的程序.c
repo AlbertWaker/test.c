1b065d345bb5ea9335eb028e9ba99815b77e3812
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-15 22:04:09 +0800

    Add files via upload

---
+++ 输入成绩，输出成绩等级的程序.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int score;
+	printf("请输入成绩:");
+	scanf("%d", &score);
+	if (score >= 90 && score <= 100)
+	{
+		printf("A");
+	}
+	else if (score >= 70 && score <= 89)
+	{
+		printf("B");
+	}
+	else if (score >= 60 && score <= 69)
+	{
+		printf("C");
+	}
+	else if (score >= 0 && score <= 59)
+	{
+		printf("D");
+	}
+	else
+	{
+		printf("Invalid score");
+	}
+	return 0;
+}
