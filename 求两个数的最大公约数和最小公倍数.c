90c3e8121074aff868796a6f9432a8aaae83ecef
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-16 19:06:57 +0800

    Add files via upload

---
+++ 求两个数的最大公约数和最小公倍数.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int r = 0;
+	int n = 0;
+	printf("请输入两个数:");
+	scanf("%d %d", &a, &b);
+	n = a * b;
+	while (r = a % b)
+	{
+		a = b;
+		b = r;
+	}
+	printf("最大公约数:%d\n", b);
+	printf("最小公倍数:%d\n", n / b);
+	return 0;
+}
