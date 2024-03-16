975500774f67df7faae007186fa36c36ed7f746f
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-16 18:54:14 +0800

    Add files via upload

---
+++ 将第一个元素移到列表的最后，然后输出这个列表.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+	int tmp = arr[0];
+	int i = 0;
+	for (i = 0; i < 9; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+	arr[9] = tmp;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
