54a5ec15b5454f94d01ce78f648a82aea87f19c7
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-19 10:36:07 +0800

    Add files via upload

---
+++ 输入一个十进制正整数n和要转换后的进制m，输出转换后的m进制数。.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>	
+#include <string.h>
+int main()
+{
+	int n = 0;
+	int m = 0;
+	int arr[20] = { 0 };
+	int i = 0;
+	printf("请输入一个十进制正整数:");
+	scanf("%d", &n);
+	printf("请输入要转换的进制(2<=m<10):");
+	scanf("%d", &m);
+	while (n)
+	{
+		arr[i] = n % m;
+		n /= m;
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		printf("%d", arr[i]);
+	}
+	return 0;
+}
