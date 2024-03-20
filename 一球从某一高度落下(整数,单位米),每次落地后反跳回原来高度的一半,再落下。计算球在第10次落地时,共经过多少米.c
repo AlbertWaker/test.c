e6f335dbca201ae3e67d89a8e8d7fc334d3bd689
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-20 15:16:46 +0800

    Add files via upload

---
+++ 一球从某一高度落下(整数,单位米),每次落地后反跳回原来高度的一半,再落下。计算球在第10次落地时,共经过多少米.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	float sum = 0;
+	float h = 0;
+	int i = 0;
+	printf("请输入高度:");
+	scanf("%f", &sum);
+	h = sum;
+	for (i = 1; i < 10; i++)
+	{
+		h = h / 2;
+		sum = sum + 2 * h;
+	}
+	printf("%f", sum);
+	return 0;
+}
