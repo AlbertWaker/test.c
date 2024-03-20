d8309ee178f8f87849657dfe7eb8d968f92291ec
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-20 15:49:10 +0800

    Add files via upload

---
+++ 因式分解：输入一个大于1的正整数n，输出它的因式分解形式.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int number = 0;
+	int i = 2;
+	printf("请输入一个大于1的正整数:");
+	scanf("%d",&number);
+	printf("%d=",number);
+	while (number)
+	{
+		if (number % i == 0)
+		{
+			if (number / i == 1)
+			{
+				number = number - i;
+				printf("%d", i);
+			}
+			else
+			{
+				number = number / i;
+				printf("%d*", i);
+			}
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return 0;
+}
