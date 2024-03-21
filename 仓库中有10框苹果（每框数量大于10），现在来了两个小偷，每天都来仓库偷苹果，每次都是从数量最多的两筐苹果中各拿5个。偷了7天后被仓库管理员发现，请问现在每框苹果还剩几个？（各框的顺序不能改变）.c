e6ca3d2a9d3e8c6f31ef35ba8bc55c6f55fb401d
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-21 20:57:09 +0800

    Add files via upload

---
+++ 仓库中有10框苹果（每框数量大于10），现在来了两个小偷，每天都来仓库偷苹果，每次都是从数量最多的两筐苹果中各拿5个。偷了7天后被仓库管理员发现，请问现在每框苹果还剩几个？（各框的顺序不能改变）.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	int max1 = 0;//最大值1
+	int max2 = 9;//最大值2
+	int days = 7;//天数
+	int i = 0;
+	printf("输入:");
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	while (days--)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (arr[i] > arr[max1] && i != max2)max1 = i;
+			if (arr[9-i] > arr[max2] && 9-i != max1)max2 = 9-i;
+		}
+		arr[max1] -= 5, arr[max2] -= 5;
+		max1 = 0, max2 = 9;
+	}
+	printf("输出:");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
