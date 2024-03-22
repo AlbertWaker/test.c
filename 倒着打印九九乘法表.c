9fc094088a0e5dfb2d463c267f9fdff025ce7ffe
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-22 22:30:38 +0800

    Add files via upload

---
+++ 倒着打印九九乘法表.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (i > j)
+			{
+				printf("\t");
+			}
+			else
+			{
+				printf("%d*%d=%d\t", i, j, i * j);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
