d5af472cba9bad7d30d8e9eec5dfe56004693ab5
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-23 12:33:23 +0800

    Add files via upload

---
+++ 输出合数的每个质数因子及该因子的个数.c
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int number = 0;
+	int count = 0;
+	int i = 2;
+	printf("请输入一个合数:");
+	scanf("%d", &number);
+	while (number)
+	{
+		if (number % i == 0)
+		{
+			if (number - i == 0)
+			{
+				number = number - i;
+				count++;
+				printf("%d %d\n", i, count);
+			}
+			else
+			{
+				number = number / i;
+				count++;
+			}
+		}
+		else
+		{
+			if (count)
+			{
+				printf("%d %d\n", i, count);
+				count = 0;
+			}
+			i++;
+		}
+	}
+}
