69c538791696a4ca4f91e2805f57a0dc913fff79
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-23 13:12:38 +0800

    Add files via upload

---
+++ 猴子吃桃：假设猴子有100个桃子，它每天根据心情吃1~5个，试问可以吃多少天.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <time.h>
+int main()
+{
+	int Peach = 100;
+	int eat = 0;
+	int day = 0;
+	srand((unsigned int)time(NULL));
+	while (Peach)
+	{
+		eat = rand() % 5 + 1;
+		if (Peach < eat)//说明：如果最后一天的桃子数不足想吃的个数，则一次性吃完。
+		{
+			Peach = 0;
+			day++;
+		}
+		else
+		{
+			Peach = Peach - eat;
+			day++;
+		}
+	}
+	printf("%d\n", day);
+	return 0;
+}
