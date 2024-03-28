0583782e10296c003f5798d9dd6929d1645d75d0
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-28 20:49:07 +0800

    Add files via upload

---
+++ 杨辉三角.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#define N 11
+int main()
+{
+	int i, j, y[N][N];
+	for (i = 1; i < N; i++)
+	{
+		y[i][1] = y[i][i] = 1;
+		for (j = 2; j <= i - 1; j++)
+			y[i][j] = y[i - 1][j] + y[i - 1][j - 1];
+	}
+	for (i = 1; i < N; i++)
+	{
+		for (j = 1; j <= i; j++)
+			printf("%4d", y[i][j]);
+		printf("\n");
+	}
+	return 0;
+}
