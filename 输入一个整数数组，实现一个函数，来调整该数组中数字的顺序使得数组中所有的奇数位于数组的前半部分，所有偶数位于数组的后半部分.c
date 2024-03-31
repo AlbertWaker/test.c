774c1ddbec23f4f604ca13693bc505cf69c24bd1
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-03-31 12:21:49 +0800

    Add files via upload

---
+++ 输入一个整数数组，实现一个函数，来调整该数组中数字的顺序使得数组中所有的奇数位于数组的前半部分，所有偶数位于数组的后半部分.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void move_odd_even(int arr[],int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		while ((left < right) && (arr[left] % 2 == 1))
+		{
+			left++;
+		}
+		while ((left < right) && (arr[right] % 2 == 0))
+		{
+			right--;
+		}
+		if (left < right)
+		{
+			int tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+			left++;
+			right--;
+		}
+	}
+}
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		scanf("%d", arr + i);
+	}
+	move_odd_even(arr,sz);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
